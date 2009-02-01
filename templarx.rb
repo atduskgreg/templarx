23dce0e3010438b4eaf6b0b73352644f8254fcbc
Devin Chalmers <devin.chalmers@gmail.com>
2009-02-01 17:54:38 +0800

    Template with array, better array initialization, some defaults
    Signed-off-by: Greg Borenstein <greg@mfdz.com>

--- templarx.rb
+++ templarx.rb
@@ -1,22 +1,24 @@
 require 'erb'
 
 class Templarx  
-  TEMPLATE_PATH = File.expand_path(File.dirname(__FILE__)) + "/drum_definition.erb"
+  TEMPLATE_PATH = File.join(File.dirname(__FILE__), '/drum_definition.erb')
 
   attr_accessor :probabilities
   attr_accessor :default_probability
+  attr_accessor :start_channel
   
   def initialize opts={}
+    opts = {:probability => 0.5,
+            :definition_path => File.join(File.dirname(__FILE__), 'test.rb'),
+            :start_channel => 36}.merge opts
     @ddpath = opts[:definition_path]
     @default_probability = opts[:probability] 
   end
 
-  # 10x16 hash of 0.0-1.0
+  # 10x16 array of 0.0-1.0
   # rows represent midi channels, columns sixteenth notes
   def initialize_probabilities
-    @probabilities = {}
-    (0..9).each{|i| @probabilities[i] = {}}
-    (0..9).each{|i| k={}; (0..15).each{|j| k[j] = @default_probability || 0.5}; @probabilities[i] = k }
+    @probabilities = (0..9).map{Array.new(16, @default_probability)}
   end
   
   def template
